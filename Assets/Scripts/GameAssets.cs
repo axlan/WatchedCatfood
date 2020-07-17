7b93957bfabf005883625e211341791c17ff2a8f
Jonathan Diamond <feros32@gmail.com>
2020-07-17 00:47:09 -0700

    Simple stop watch

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,29 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameAssets : MonoBehaviour
+{
+
+    public static GameAssets i;
+
+    void Start() {
+        i = this;
+    }
+
+    public Text timerText;
+
+}
